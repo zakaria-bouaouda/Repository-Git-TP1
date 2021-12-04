378da79ac0b2bc58d500b4ae0957ccafb1b75f02
zakaria-bouaouda <zakariaouaouda1@gmail.com>
2021-12-04 23:23:06 +0100

    this is first commit from zakaria

---
+++ TP_GIT/src/main/java/ma/ginfo/bouaouda/TpGitApplication.java
@@ -0,0 +1,13 @@
+package ma.ginfo.bouaouda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGitApplication.class, args);
+	}
+
+}
